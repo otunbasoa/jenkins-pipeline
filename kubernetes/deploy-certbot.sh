9cbefd875ddd9658a34524a7038ceb1ab23c1c2a
otunba <anjorinolu.samson@gmail.com>
2023-03-18 16:47:06 +0100

    cerbot

--- kubernetes/deploy-certbot.sh
+++ kubernetes/deploy-certbot.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Install Certbot and the Nginx plugin
-kubectl run certbot --image=certbot/certbot:v1.23.0 --restart=Never -- /bin/sh -c "apk add --no-cache nginx; apk add --no-cache certbot; apk add --no-cache certbot-nginx"
+POD_NAME="certbot"
+NAMESPACE="default"
 
-# Obtain a certificate from Let's Encrypt using Certbot and the Nginx plugin
-kubectl run certbot --image=certbot/certbot:v1.23.0 --restart=Never -- /bin/sh -c "certbot --nginx -d anjorin.me -d www.anjorin.me -d sockapp.anjorin.me -d grafana.anjorin.me -d prometheus.anjorin.me -d myprofile.anjorin.me"
+# Install Certbot and Nginx in a new pod
+kubectl run "${POD_NAME}" --image=certbot/certbot:v1.23.0 --restart=Never -- /bin/sh -c "apk add --no-cache nginx; apk add --no-cache certbot; apk add --no-cache certbot-nginx"
+
+# Get the name of the nginx container in the pod
+CONTAINER_NAME=$(kubectl get pods "${POD_NAME}" -n "${NAMESPACE}" -o jsonpath='{.spec.containers[*].name}' | tr ' ' '\n' | grep nginx)
+
+# Run certbot in the nginx container
+kubectl exec "${POD_NAME}" -n "${NAMESPACE}" -c "${CONTAINER_NAME}" -- certbot --nginx -d anjorin.me -d www.anjorin.me -d sockapp.anjorin.me -d grafana.anjorin.me -d prometheus.anjorin.me -d myprofile.anjorin.me
 
 # Restart Nginx to apply the changes
-kubectl exec -it certbot -- /bin/sh -c "nginx -s reload"
+kubectl exec "${POD_NAME}" -n "${NAMESPACE}" -c "${CONTAINER_NAME}" -- nginx -s reload
